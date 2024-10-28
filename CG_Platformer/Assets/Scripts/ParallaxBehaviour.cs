238a5a7cd2bef421079da07d6cf6a91ac8a184a1
Martin <mvrt.new@gmail.com>
2024-10-28 15:14:38 +0300

    V4

---
+++ CG_Platformer/Assets/Scripts/ParallaxBehaviour.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBehaviour : MonoBehaviour
+{
+    [SerializeField] Transform followingTarget;
+    [SerializeField, Range(0f, 1f)] float parallaxStrength = 0.1f;
+    [SerializeField] bool disableVerticalParallax;
+    Vector3 targetPreviousPosition;
+
+    void Start()
+    {
+        if(!followingTarget)
+        {
+            followingTarget = Camera.main.transform;
+        }
+
+        targetPreviousPosition = followingTarget.position;
+    }
+
+    
+    void Update()
+    {
+        Vector3 delta = followingTarget.position - targetPreviousPosition;
+
+        if (disableVerticalParallax)
+        {
+            delta.y = 0;
+        }
+
+        targetPreviousPosition = followingTarget.position;
+
+        transform.position += delta * parallaxStrength;
+    }
+}
