8c39b284b3bb904dba81655f9fb8091893feefc5
Martin <mvrt.new@gmail.com>
2024-11-03 15:33:01 +0300

    V5

--- CG_Platformer/Assets/Scripts/PlayerController.cs
+++ CG_Platformer/Assets/Scripts/PlayerController.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
+    CircleCollider2D legs;
 
     [SerializeField] float speed = 5f;
+    [SerializeField] float jumpForce = 8f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        legs = GetComponent<CircleCollider2D>();
     }
 
 
     void Update()
     {
         Move();
+        Jump();
         FlipCharacter();
     }
 
@@ -25,6 +29,21 @@ public class PlayerController : MonoBehaviour
     {
         float newX = Input.GetAxis("Horizontal") * speed;
         rb.velocity = new Vector2(newX, rb.velocity.y);
+    }
+
+
+    void Jump()
+    {
+        LayerMask ground = LayerMask.GetMask("Ground");
+        bool isGrounded = legs.IsTouchingLayers(ground);
+
+        if (isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            }
+        }
     }
 
 
